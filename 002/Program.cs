64fcf18c59972bcdb792cd9297cdc856b87cae0c
Shakhemir <shakhemir@gmail.com>
2022-02-22 21:32:03 +0300

    Еще пару прог

---
+++ 002/Program.cs
@@ -0,0 +1,8 @@
+﻿double number1 = new Random().Next(5, 15);
+double number2 = new Random().Next(1, 10);
+double result = number1 / number2;
+Console.Write(number1);
+Console.Write(" / ");
+Console.Write(number2);
+Console.Write(" = ");
+Console.WriteLine(result);
